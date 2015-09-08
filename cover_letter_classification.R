8e60cad8034acd3cb07ff162eedba4549d2f1d5e
fineiskid <ffineis@uw.edu>
2015-09-08 18:13:14 -0500

    test classification function complete

--- cover_letter_classification.R
+++ cover_letter_classification.R
@@ -85,19 +85,19 @@ text_cleaner <- function(text){
 
 
 #get dictionary of all words for certain class
-text_for_class <- function(class = 0, name_class_stor=name_class_stor){
+text_for_class <- function(class = 0, ncs=name_class_stor){
   '
   ARGS:
   class (int): indicates success (1) or failure (0) of hearing back given a cover letter
   name_class_stor (matrix): 2-column matrix with filenames (one column) and document class (second column)
   '
   class_text = vector(mode = 'character'); ctr = 1
-  files <- name_class_stor[which(name_class_stor[,2]==class),]
+  files <- ncs[which(ncs[,2]==class),]
   for (ii in 1:nrow(files)){
     print(paste("Attempting to read file", files[ii,1], sep = " "))
     f = read_file(files[ii,1])
     if (is.null(f)){
-      name_class_stor <- name_class_stor[which(name_class_stor[,1]!=files[ii,1]),]}
+      ncs <- ncs[which(ncs[,1]!=files[ii,1]),]}
     if (!is.null(f)){
       dims = dim(f)
       for (k in 1:dims[1]){
@@ -109,8 +109,8 @@ text_for_class <- function(class = 0, name_class_stor=name_class_stor){
       }
     }
   }
-  name_class_stor <- name_class_stor[which(name_class_stor[,2]==class),]
-  return(list(text = class_text[class_text!=""], name_class_stor = name_class_stor))
+  ncs <- ncs[which(ncs[,2]==class),]
+  return(list(text = class_text[class_text!=""], name_class_stor = ncs))
 }
 
 #list of all words used, per class, with repetitions allowed
@@ -122,15 +122,15 @@ name_class_stor <- rbind(c0$name_class_stor, c1$name_class_stor)
 V <- unique(c(c0_tokens, c1_tokens))
 
 #count token occurences, according to class:
-reduce <- function(class_tokens, V=V){
+reduce <- function(class_tokens, V_vec=V){
   '
   ARGS:
   class_tokens (char vector): the non-unique vector of word occurences pertaining to a certain class
   V (char vector): entire vocabulary
   '
-  keys <- V
+  keys <- V_vec
   vals <- NULL
-  for (k in 1:length(V)){
+  for (k in 1:length(V_vec)){
     vals <- c(vals, sum(class_tokens==keys[k]))}
   return(as.data.frame(cbind(keys, as.numeric(vals))))
 }
@@ -147,10 +147,10 @@ condprob[,2] <- (as.numeric(counts_1[,2]) + 1)/(S1 + length(V))
 
 p1 <- sum(as.numeric(name_class_stor[,2]))/nrow(name_class_stor)
 p0 <- sum(as.numeric(name_class_stor[,2]==0))/nrow(name_class_stor)
-priors <- c(p1,p0)
+priors <- c(p0,p1)
 
 
-test_doc_classify <- function(file, V=V, condprob = condprob, priors = priors){
+test_doc_classify <- function(file, V_vec=V, cp = condprob, p = priors){
   '
   ARGS:
   file (char): filename of test document we wish to classify
@@ -158,10 +158,41 @@ test_doc_classify <- function(file, V=V, condprob = condprob, priors = priors){
   condprob (numeric 2-d array): class conditional probabilities of each word
   priors (numeric vector): class priors
   '
-  
-
-
+  f <- read_file(file)
+  t <- NULL
+  if (!is.null(f)){
+    dims = dim(f)
+    for (k in 1:dims[1]){
+      for (j in 1:dims[2]){
+        #turn this into a function to clean text
+        text = text_cleaner(f[k,j])
+        t <- c(t, text)
+      }
+    }
+    t <- unique(t[which(t %in% V_vec)])
+    scores <- NULL
+    for (c in 0:1){
+      loglik <- 0
+      log_prior <- log(p[c+1])
+      loglik <- loglik + log_prior
+      for (word in 1:length(t)){
+        loglik <- loglik + log(cp[which(rownames(cp)==t[word]),c+1])
+      }
+      scores <- c(scores, loglik)
+    }
+    scores <- as.data.frame(t(scores))
+    colnames(scores) <- c('0', '1')
+    cl <- test_doc_classify(newfile)
+    cat("Most likely class of new file: ", cl)
+    return(as.numeric(names(which.max(scores))))
+  }
+  else{
+    print('Something went wrong while reading this file!\n')
+    return(NULL)
+  }
 }
+
+
 
 
 
